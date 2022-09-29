8740e21da035041a14d4514289563393e9b72779
Paulius <paulius.truikys@gmail.com>
2022-09-29 21:15:24 +0300

    UI: list all books.

--- BooksSpot2022/ClientApp/src/components/Home.js
+++ BooksSpot2022/ClientApp/src/components/Home.js
@@ -1,26 +1,81 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = { books: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.populateBooksData();
+    }
+
+    static statusToString(bookStatus) {
+        if (bookStatus == 0) {
+            return 'Available';
+        } else if (bookStatus == 1) {
+            return 'Reserved';
+        } else if (bookStatus == 2) {
+            return 'Borrowed';
+        }
+        return bookStatus;
+    }
+
+    static renderBooksTable(books) {
+        return (
+            <table className='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Publisher</th>
+                        <th>Publishing Date</th>
+                        <th>Genre</th>
+                        <th>ISBN</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books.map(book =>
+                        <tr key={book.id}>
+                            <td>{book.title}</td>
+                            <td>{book.author}</td>
+                            <td>{book.publisher}</td>
+                            <td>{book.publishingDate}</td>
+                            <td>{book.genre}</td>
+                            <td>{book.isbn}</td>
+                            <td>{Home.statusToString(book.status)}</td>
+                            <td>
+                                <button type="button" class="btn btn-secondary m-1">Reserve</button>
+                                <button type="button" class="btn btn-primary m-1">Borrow</button>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Home.renderBooksTable(this.state.books);
+
+        return (
+            <div>
+                <h1>Books</h1>
+                {contents}
+            </div>
+        );
+    }
+
+    async populateBooksData() {
+        const response = await fetch('books');
+        const data = await response.json();
+        console.log(data);
+        this.setState({ books: data, loading: false });
+    }
 }
